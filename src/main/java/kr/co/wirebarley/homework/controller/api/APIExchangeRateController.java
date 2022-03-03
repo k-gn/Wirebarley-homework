675ed3ee4e717d7f5a98ccfb90f58419e9bc5c87
k-gn <rlarbska97@gmail.com>
2022-03-03 22:21:55 +0900

    API 컨트롤러 추가 / Validation 검증

--- src/main/java/kr/co/wirebarley/homework/controller/api/APIExchangeRateController.java
+++ src/main/java/kr/co/wirebarley/homework/controller/api/APIExchangeRateController.java
@@ -1,12 +1,30 @@
 package kr.co.wirebarley.homework.controller.api;
 
+import kr.co.wirebarley.homework.dto.exchange.ExchangeRequest;
 import kr.co.wirebarley.homework.service.ExchangeRateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/exchange")
 public class APIExchangeRateController {
 
     private final ExchangeRateService exchangeRateService;
+
+    @GetMapping("/today")
+    public ResponseEntity<?> getTodayExchangeRate(@Valid @RequestBody ExchangeRequest exchangeRequest) {
+
+        return ResponseEntity.ok(exchangeRateService.getTodayExchangeRate(exchangeRequest));
+    }
+
+    @GetMapping("/convert")
+    public ResponseEntity<?> convertToExchangeRate(@Valid @RequestBody ExchangeRequest exchangeRequest) {
+
+        return ResponseEntity.ok(exchangeRateService.convertToExchangeRate(exchangeRequest));
+    }
+
 }
--- src/main/java/kr/co/wirebarley/homework/dto/exchange/ExchangeRequest.java
+++ src/main/java/kr/co/wirebarley/homework/dto/exchange/ExchangeRequest.java
@@ -3,12 +3,22 @@ package kr.co.wirebarley.homework.dto.exchange;
 import kr.co.wirebarley.homework.constant.Country;
 import lombok.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ExchangeRequest {
 
+    @NotBlank(message = "송금액이 바르지 않습니다.")
+    @Max(value = 10000, message = "송금액이 바르지 않습니다.")
+    @Min(value = 0, message = "송금액이 바르지 않습니다.")
     private Long exchangeMoney;
+
+    @NotNull(message = "수취국가를 선택해주세요.")
     private Country country;
 }
