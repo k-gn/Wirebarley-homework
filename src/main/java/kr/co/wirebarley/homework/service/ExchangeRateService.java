445da928efcf071ab4d703323be92c5448dfbbc0
k-gn <rlarbska97@gmail.com>
2022-03-04 19:54:23 +0900

    정리

--- src/main/java/kr/co/wirebarley/homework/service/ExchangeRateService.java
+++ src/main/java/kr/co/wirebarley/homework/service/ExchangeRateService.java
@@ -1,10 +1,7 @@
 package kr.co.wirebarley.homework.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.co.wirebarley.homework.dto.exchange.ExchangeRequest;
 import kr.co.wirebarley.homework.dto.exchange.ExchangeResponse;
-import kr.co.wirebarley.homework.dto.exchange.ExchangeResult;
-import kr.co.wirebarley.homework.dto.exchange.ExchangeResultTo;
 
 public interface ExchangeRateService {
 
